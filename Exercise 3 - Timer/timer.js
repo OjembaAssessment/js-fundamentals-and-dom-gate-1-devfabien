5fc5113bbc379da183c27d4de6af5fbef411b243
Fabien <ish001fabi@gmail.com>
2023-07-24 12:48:11 +0200

    adding timer functionality

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,5 +1,6 @@
 let count=0;
 const value= document.querySelector("#num");
+let start=document.querySelector("#start");
 const btns=document.querySelectorAll('.btn');
 btns.forEach(function(btn){
     btn.addEventListener('click',function(e){
@@ -12,5 +13,25 @@ btns.forEach(function(btn){
             count=0;
         }
         value.textContent=count;
+        let vvalue=value.innerHTML;
+        
+        start.addEventListener('click', function (){
+            var stopWatch = setInterval(function() { 
+            if(vvalue <0){     
+                count=0 ;
+                clearInterval(stopWatch);         
+                return;
+            }
+            document.getElementById("num").innerHTML = vvalue;
+            start.textContent="stop";
+            if(vvalue==0){
+                start.textContent="start"
+            }
+            vvalue--;
+        }, 1000);
+        vvalue.innerHTML=0;
+        })
     })
 })
+
+
