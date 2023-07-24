8d8a23195402e3d91ad532b273b2fd6118864ddf
Fabien <ish001fabi@gmail.com>
2023-07-24 10:28:29 +0200

    getting the top five most occured words

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,17 @@
-console.log("Exercise 1 - Word Highlighter");
+
+let paragraph= document.getElementById('myParagraph');
+let splitted=paragraph.split(' ');
+  let wordMap = {};
+  for (var i = 0; i < splitted.length; i++) {
+    let currentWordCount = wordMap[splitted[i]];
+    let count = currentWordCount ? currentWordCount : 0;
+    wordMap[splitted[i]] = count + 1;
+  }
+  console.log (Object.values(wordMap).sort((a, b)=>b - a)).slice(0,5);
+ let kkeys= Object.keys(wordMap).sort((a, b)=>wordMap[b] - wordMap[a]).slice(0,5);
+
+
+
+
+
+
