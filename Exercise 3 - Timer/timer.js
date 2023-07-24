30b2414495220ccc7b16362d065235827afc2eec
Fabien <ish001fabi@gmail.com>
2023-07-24 12:19:48 +0200

    counter setup

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,16 @@
-console.log("Exercise 3 - Timer");
+let count=0;
+const value= document.querySelector("#num");
+const btns=document.querySelectorAll('.btn');
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('remove')){
+            count--;
+        }else if(styles.contains('add')){
+            count++;
+        }else{
+            count=0;
+        }
+        value.textContent=count;
+    })
+})
